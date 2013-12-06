0f5c49a459381b54f3da0e43b605ea915a3109eb
Jay Bromley <jbromley@gmail.com>
2013-12-06 00:33:53 -0800

    Added debouncing and multiple LEDs.
    Blink two LEDs. Add a button push with debouncing to slow down
    the blink time.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -3,20 +3,63 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
  
   This example code is in the public domain.
- */
+*/
+#include "Debouncer.h"
 
-void setup() {                
-  // initialize the digital pin as an output.
-  // Pin 14 has an LED connected on most Arduino boards:
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);  
+// 
+// Minimum and maximum turn-on times for LEDs.
+const int MIN_DELAY_TIME = 100;
+const int MAX_DELAY_TIME = 1000;
+
+// Current delay time.
+int delayTime = 100;
+
+// Button debouncer
+Debouncer pushButton(PUSH2, 50);
+
+// Have we incremented delay for current button push.
+boolean incremented = false;
+
+// Last update time for LEDs.
+unsigned long previousUpdate;
+int ledState = LOW;
+
+int invertState(int state)
+{
+    return state == LOW ? HIGH : LOW;
+}
+
+void setup() 
+{                
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);  
+
+    pushButton.update();
+
+    previousUpdate = millis();
+    digitalWrite(RED_LED, ledState);
+    digitalWrite(GREEN_LED, invertState(ledState));
 }
 
 void loop() {
-  digitalWrite(RED_LED, HIGH);   // set the LED on
-  digitalWrite(GREEN_LED, LOW);
-  delay(500);              // wait for a second
-  digitalWrite(RED_LED, LOW);    // set the LED off
-  digitalWrite(GREEN_LED, HIGH);
-  delay(500);              // wait for a second
+    int buttonState = pushButton.update();
+    if (buttonState == LOW) {
+	if (!incremented) {
+	    delayTime += 100;
+	    if (delayTime > MAX_DELAY_TIME) {
+		delayTime = MIN_DELAY_TIME;
+	    }
+	    incremented = true;
+	} 
+    } else {
+	incremented = false;
+    }
+
+    unsigned long currentTime = millis();
+    if (currentTime - previousUpdate > delayTime) {
+	previousUpdate = currentTime;
+	ledState = invertState(ledState);
+	digitalWrite(RED_LED, ledState);
+	digitalWrite(GREEN_LED, invertState(ledState));
+    }
 }
---
+++ Blink/Debouncer.cpp
@@ -0,0 +1,29 @@
+// Debouncer.cpp
+// Debounce a button read.
+#include "Debouncer.h"
+
+Debouncer::Debouncer(int button, long debounceDelay /* = 25 */)
+    : m_button(button), 
+      m_buttonState(HIGH), 
+      m_lastButtonState(HIGH), 
+      m_debounceDelay(debounceDelay)
+{
+    pinMode(button, INPUT_PULLUP);
+}
+
+int Debouncer::update()
+{
+    int readState = digitalRead(m_button);
+
+    if (readState != m_lastButtonState) {
+	m_lastDebounceTime = millis();
+    }
+
+    if ((millis() - m_lastDebounceTime) > m_debounceDelay) {
+	m_buttonState = readState;
+    }
+
+    m_lastButtonState = readState;
+
+    return m_buttonState;
+}
